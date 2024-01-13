b3fda7dd1115bfa9ff702f36ae6b8315c183d62f
Gbadebo Oluwarimi <oluwarimigbadebo@gmail.com>
2024-01-12 17:25:04 -0800

    Nerver  giving up

--- app/page.js
+++ app/page.js
@@ -154,7 +154,7 @@ export default function Home() {
       <div className="pt-20 max-w-5xl m-auto">
         <div className="flex ">
           <div className="pb-4">
-            <div className="text-2xl pb-4" style={gellix13.style}>
+            <div className="text-4xl pb-4" style={gellix13.style}>
               About Me
               <div className="w-20 h-1 bg-lightgreen mt-2"></div>
             </div>
@@ -178,14 +178,110 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="pt-20 max-w-4xl m-auto">
-          <div className="text-2xl pb-4 " style={gellix13.style}>
+        <div className="pt-20 max-w-5xl m-auto">
+          <div className="text-4xl pb-4 " style={gellix13.style}>
             Work Experience
-            <div className="w-20 h-1 bg-lightgreen mt-2"></div>
+            <div className="w-32 h-1 bg-lightgreen mt-2"></div>
           </div>
-          <div>Freelancer</div>
-          <div>Alps Finance</div>
-          <div>Picotel network Solution</div>
+          <div className="pt-10">
+            <div className="flex">
+              <div className="flex max-w-3xl m-auto">
+                <div className=" inline-block items-center justify-center text-center">
+                  <Image
+                    src="/alps.jpg"
+                    width={180}
+                    height={180}
+                    className="rounded-sm"
+                  />
+                  <div className="border-l border-lightgreen min-h-32 ml-9 mt-1" />
+                  <div className="p-2 rounded-full bg-lightgreen w-4 h-4 ml-7 " />
+                  -
+                </div>
+                <div className="px-4">
+                  <span className="text-lightgreen text-sm font-semibold">
+                    Intern
+                  </span>
+                  <div className="text-xl font-semibold pb-2">
+                    Alps Finance (Full Stack Engineer)
+                  </div>
+                  <span className="text-sm ">AUG, 5 2019 -- PRESENT</span>
+                  <div className="text-sm pt-4">
+                    Revitalized Alps Finance website, elevating standards and
+                    significantly increasing user retention. Acquired valuable
+                    Web 3 insights, showcasing adaptability and a keen grasp of
+                    innovative technologies. Ready to bring this expertise and
+                    enthusiasm to your team
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* Sectioin for Picotel Network Solutions */}
+            <div className="flex max-w-3xl m-auto mt-2">
+              <div className=" inline-block items-center justify-center text-center">
+                <Image
+                  src="/logoxl.png"
+                  width={180}
+                  height={180}
+                  className="rounded-sm"
+                />
+                <div className="border-l border-lightgreen h-4/5 mt-3 ml-9" />
+              </div>
+              <div className="px-4">
+                <span className="text-lightgreen text-sm font-semibold">
+                  Full-Stack Engineer
+                </span>
+                <div className="text-xl font-semibold pb-2">
+                  Picotel Network Solutions (Full Stack Engineer)
+                </div>
+                <span className="text-sm ">AUG, 5 2019 -- AUG, 5 2021</span>
+                <ul className="text-sm pt-8 list-disc">
+                  <li className="pb-4">
+                    I commenced my journey at Picotel as an intern Network
+                    Engineer, demonstrating proficiency that led to my
+                    transition to a Full Stack Engineer. During my tenure, I
+                    spearheaded the development of in-house systems, showcasing
+                    my ability to contribute strategically.
+                  </li>
+                  <li className="pb-4">
+                    Recognizing my dedication and accomplishments, I was
+                    promoted to a senior role within the Full Stack Development
+                    team. This progression underscores my commitment to
+                    continuous growth and my capacity to deliver impactful
+                    solutions within a professional setting.
+                  </li>
+                  <li className="pb-4">
+                    Notably, I developed an in-house invoice generator and
+                    reminder system, demonstrating both technical prowess and
+                    practical contributions to enhance company operations. This
+                    aligns with my commitment to advancing both my skills and
+                    the organization's technological capabilities.
+                  </li>
+                  <li className="pb-4">
+                    Through my software contributions, we achieved operational
+                    excellence, enabling a 20% company expansion in 2022. The
+                    impact on service quality and efficiency showcases the
+                    tangible results of my work.
+                  </li>
+                  <li className="pb-4">
+                    Simultaneously, our user retention time on the site saw a
+                    significant boost, increasing by 15% in the same month.
+                    <br />
+                    Recognizing my dedication and accomplishments, I was
+                    promoted to a senior role within the Full Stack Development
+                    team
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="text-3xl font-semibold">Some things i've built</div>
+            <div className="w-32 h-1 bg-lightgreen mt-2"></div>
+            <div className="inline-block md:flex">
+              <div></div>
+              <div></div>
+            </div>
+          </div>
         </div>
       </div>
     </main>
